812e34b61d1a06bc4cb9f5e87dcfe233a061fc18
Jesse <jesse_11biscuit@hotmail.com>
2022-11-24 21:25:13 +1030

    fixed paths

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,11 @@ const router = require('express').Router();
 
 
 router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 router.post('/api/notes', (req, res) => {
-    let database = fs.readFileSync('db/db.json');
+    let database = fs.readFileSync('./db/db.json');
     database = JSON.parse(database);
     res.json(database);
 
@@ -20,14 +20,14 @@ router.post('/api/notes', (req, res) => {
     };
 
     database.push(note);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
     res.json(database);
 });
 
 router.delete('/api/notes/:id', (req, res) => {
-    let database = JSON.parse(fs.readFileSync('db/db.json'))
+    let database = JSON.parse(fs.readFileSync('./db/db.json'))
     let deleteNotes = database.filter(item => item.id !== req.params.id);
-    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes));
     res.json(deleteNotes);
     
 });
